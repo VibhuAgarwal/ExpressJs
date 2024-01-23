7925e235b00af12f57161e63df8a1e042bc5655a
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-01-23 21:14:41 +0530

    Added session, cart data pull api

--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,8 +1,38 @@
 export const mockUsers = [
-  { id: 1, username: "Vibhor Agarwal", displayName: "Vibhor" },
-  { id: 2, username: "Deepak Singh", displayName: "Depepu" },
-  { id: 3, username: "Praveen Singh", displayName: "Draco" },
-  { id: 4, username: "Tibhor Agarwal", displayName: "Tibhor" },
-  { id: 5, username: "Geepak Singh", displayName: "Gepepu" },
-  { id: 6, username: "Oraveen Singh", displayName: "" },
+  {
+    id: 1,
+    username: "s1mple",
+    displayName: "Vibhor Agarwal",
+    password: "hello123",
+  },
+  {
+    id: 2,
+    username: "deepu",
+    displayName: "Deepak Singh",
+    password: "hello123",
+  },
+  {
+    id: 3,
+    username: "draco",
+    displayName: "Praveen Singh",
+    password: "hello123",
+  },
+  {
+    id: 4,
+    username: "tibhor",
+    displayName: "Tibhor Agarwal",
+    password: "hello123",
+  },
+  {
+    id: 5,
+    username: "geez",
+    displayName: "Geez patil",
+    password: "hello123",
+  },
+  {
+    id: 6,
+    username: "orri",
+    displayName: "Orri Singh",
+    password: "hello123",
+  },
 ];
